e29167d305c054bd8f0e32efe0ae49d6638e041c
Sumit Mandal <sumit.mandal0123@gmail.com>
2026-05-25 23:09:02 +0530

    Add Entities

---
+++ eCommerceSolution.OrdersService/Models/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace eCommerceSolution.OrdersService.Models.Entities;
+
+public class Order
+{
+    public Guid OrderId { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalBill { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+}
---
+++ eCommerceSolution.OrdersService/Models/Entities/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace eCommerceSolution.OrdersService.Models.Entities;
+
+public class OrderItem
+{
+    public Guid ProductId { get; set; }
+    public decimal UnitPrice { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+}
